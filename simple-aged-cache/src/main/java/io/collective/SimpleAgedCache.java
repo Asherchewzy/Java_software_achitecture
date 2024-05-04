142cfae284e34f3d13bdeffbcb58118512e60751
Asher Chew AIAP <asher_chew@aiap.sg>
2024-05-04 14:49:59 +0800

    populate some func, incomplete

--- simple-aged-cache/src/main/java/io/collective/SimpleAgedCache.java
+++ simple-aged-cache/src/main/java/io/collective/SimpleAgedCache.java
@@ -6,31 +6,34 @@ import java.util.HashMap;
 public class SimpleAgedCache {
     private Clock clock; //similar to a self.__var uder def__init__(self) in py
     private MAP<Object, Object> cache; //dict in py
-    private MAP<Object, Short> expirations; //retension in ms goes up to 4000 only
+    private MAP<Object, Long> expirations; //retension in ms goes up to 4000 only but then clock.millis is a long type
 
 
     public SimpleAgedCache(Clock clock) {
         this.clock = clock; //init the clock with clock param, this = similar to self in py
+    }
+
+    public SimpleAgedCache() {
         this.cache = new HashMap<>();
         this.expirations = new HashMap<>();
     }
 
-    public SimpleAgedCache() {
-    }
-
     public void put(Object key, Object value, int retentionInMillis) {
+        cache.put(key, value);
+        expirations.put(key, clock.millis() + retentionInMillis);
     }
 
     public boolean isEmpty() {
-        return false;
+        return cache.isEmpty(); //map has an inbuilt isEmpty() method
     }
 
     public int size() {
-        return 0;
+        return cache.size() //map has an inbuilt size() method 
     }
 
     public Object get(Object key) {
-        return null;
+        Object val = cache.get(key)
+        return val;
     }
 }
 
